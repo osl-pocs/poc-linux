4e26a4b51b947bd25d47044615ade735cbe5cf32
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2024-04-25 15:54:39 -0400

    fix small issues; add script to install git prompt plugin

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -17,7 +17,7 @@ done
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -29,12 +29,12 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
 ## POST INSTALL STEP ##
 
-sudo groupadd docker
+sudo groupadd docker || true
 
 sudo usermod -aG docker $USERNAME
 
